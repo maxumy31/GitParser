0ce5078099811d6e1a2c40313dca0a94a75efbe6
maximka <officialmaximka@gmail.com>
2026-01-27 16:45:05 +0300

    imporving ui

--- Server/index.js
+++ Server/index.js
@@ -33,10 +33,158 @@ fastify.get('/', async (req, reply) => {
 
 fastify.get('/search-page', async (req, reply) => {
     try {
+        console.log(req.query,"/search-page")
         const tags = await queries.GetPopularTags()
         const languages = await queries.GetSupportedLanguages()
         const sources = await queries.GetSupportedSources()
-        return reply.view('pages/search_page/search.ejs', { searchPageData: {popularTags:tags,languages:languages, sources:sources} });
+        return reply.view('pages/search_page/index.ejs', { searchPageData: {popularTags:tags,languages:languages, sources:sources} });
+    } catch (err) {
+        fastify.log.error(err);
+        return "<html>ERROR</html>"
+    }
+});
+
+
+fastify.get('/search-page/active_stack_block/standard', async (req, reply) => {
+    const standardValues = {activeStack: [] }
+    try {
+        return reply.view('pages/search_page/sidebar/active_stack_block.ejs', standardValues);
+    } catch (err) {
+        fastify.log.error(err);
+        return "<html>ERROR</html>"
+    }
+});
+
+fastify.get('/search-page/active_stack_block/add', async (req, reply) => {
+    console.log(req.query,"/search-page/active_stack_block/add")
+    let selectedStack = [].concat(req.query.activeStack || []);
+    const stackToInclude = req.query.toInclude
+    if(selectedStack.indexOf(stackToInclude) == -1) {
+        selectedStack.push(stackToInclude)
+    }
+    try {
+        reply.header("HX-Trigger-After-Swap","updateSearchResult")
+        return reply.view('pages/search_page/sidebar/active_stack_block.ejs', {activeStack: selectedStack});
+    } catch (err) {
+        fastify.log.error(err);
+        return "<html>ERROR</html>"
+    }
+});
+
+fastify.get('/search-page/active_stack_block/remove', async (req, reply) => {
+    console.log(req.query,"/search-page/active_tags_block/remove")
+    const selectedStack = [].concat(req.query.activeStack || []);
+    const stackToDisable = req.query.toDelete
+    const newSelectedStack = selectedStack.filter(item => item != stackToDisable)
+    try {
+        reply.header("HX-Trigger-After-Swap","updateSearchResult")
+        return reply.view('pages/search_page/sidebar/active_stack_block.ejs', {activeStack: newSelectedStack});
+    } catch (err) {
+        fastify.log.error(err);
+        return "<html>ERROR</html>"
+    }
+});
+
+
+fastify.get("/search_page/stack_list", async (req, reply) => {
+    console.log(req.query,"/search_page/stack_list")
+    try {
+        reply.header("HX-Trigger-After-Swap","updateSearchResult")
+        const selectedStack = [].concat(req.query.activeStack || []);
+        if(selectedStack.indexOf(req.query.stack) != -1) {return reply.code(204).send("")}
+        return reply.view('pages/search_page/sidebar/active_data/tags_list.ejs', {activeStack:[req.query.stack]});
+    } catch (err) {
+        fastify.log.error(err);
+        return "<html>ERROR</html>"
+    }
+})
+
+
+
+
+fastify.get("/search_page/tags_list_modal", async (req, reply) => {
+    try {
+        console.log(req.query,"/search_page/tags_list_modal")
+        reply.header("HX-Trigger-After-Swap","updateSearchResult")
+        const hints = (await queries.GetTagHint(req.query.tag_name,[])).map(h => h.name)
+        return reply.view('pages/search_page/tags_list_modal.ejs', {tags:hints});
+    } catch (err) {
+        fastify.log.error(err);
+        return "<html>ERROR</html>"
+    }
+})
+
+fastify.get("/search_page/tags_list", async (req, reply) => {
+    console.log(req.query,"/search_page/tags_list")
+    try {
+        reply.header("HX-Trigger-After-Swap","updateSearchResult")
+        const selectedTags = [].concat(req.query.activeTags || []);
+        if(selectedTags.indexOf(req.query.tag) != -1) {return reply.code(204).send("")}
+        return reply.view('pages/search_page/sidebar/active_data/tags_list.ejs', {activeTags:[req.query.tag]});
+    } catch (err) {
+        fastify.log.error(err);
+        return "<html>ERROR</html>"
+    }
+})
+
+fastify.get('/search_page/search_trigger', async (req, reply) => {
+    try {
+        reply.header("HX-Trigger-After-Swap", "updateSearchResult");
+        return reply.code(200).send("");
+        
+    } catch (err) {
+        fastify.log.error(err);
+        return reply.code(500).send("ERROR");
+    }
+});
+
+
+fastify.get('/search-page/search_results_list', async (req, reply) => {
+    try {
+        console.log(req.query, "/search-page/search_results_list");
+
+        const selectedSources = [].concat(req.query.sources || []);
+        const selectedLanguage = req.query.language;
+        const selectedTags = [].concat(req.query.activeTags || []);
+        const selectedDeps = [].concat(req.query.activeStack || []);
+        const search = req.query.userSearch
+
+        if (!selectedLanguage || selectedSources.length === 0) {
+            return ""; 
+        }
+
+        const limit = 10;
+        const offset = 0;
+
+        console.log(selectedDeps,selectedTags,selectedSources)
+
+        const res = await queries.GetFullRecommendations(selectedLanguage, selectedDeps, selectedTags, selectedSources, limit, offset,search);
+
+        return reply.view('pages/search_page/search_results_list.ejs', { 
+            searchResult: {
+                count: res.total, 
+                names: res.data.map(r => r.name) 
+            } 
+        });
+
+    } catch (err) {
+        fastify.log.error(err);
+        return reply.code(500).send("Ошибка при загрузке результатов");
+    }
+});
+
+
+fastify.get('/search-page/active_tags', async (req, reply) => {
+    try {
+        console.log(req.query)
+        const selectedSources = [].concat(req.query.sources || []);
+        if(selectedSources.length == 0) {//Ничего не указано в источниках
+            return ""                    //Ничего не возвращаем
+        }
+        const selectedLanguage = req.query.language
+        const res = await queries.GetRecommendationsByStack(["fastapi"],selectedLanguage,selectedSources)
+        return reply.view('pages/search_page/search_result_block.ejs', { searchResult: {count:1488,names:res.map(r => r.name)} });
+
     } catch (err) {
         fastify.log.error(err);
         return "<html>ERROR</html>"
--- Server/queries.js
+++ Server/queries.js
@@ -1,27 +1,34 @@
 import executeQuery from "./db.js";
+import Knex from 'knex';
+
+const knex = Knex({
+  client: 'pg'
+});
 
 const GetSupportedLanguages = async () => {
     const rows = await executeQuery(`
-        SELECT l.name AS language_name, COUNT(r.id) AS repo_count
-        FROM languages l
-        LEFT JOIN repositories r ON l.id = r.language_id
-        GROUP BY l.id, l.name
-        ORDER BY repo_count DESC;
+        SELECT l.name, r_counts.population
+        FROM (
+            SELECT language_id, COUNT(*) as population
+            FROM repositories
+            GROUP BY language_id
+        ) r_counts
+        JOIN languages l ON l.id = r_counts.language_id
+        ORDER BY population DESC;
     `, []);
-    return rows.map(row => row.language_name)
+    return rows.map(row => row.name)
 };
 
 const GetPopularTags = async () => {
     const rows = await executeQuery(`
-        SELECT e.name AS topic_name, COUNT(re.repo_id) AS usage_count
-        FROM entities e
-        JOIN repo_entities re ON e.id = re.entity_id
-        WHERE e.is_topic = TRUE
-        GROUP BY e.id, e.name
+        SELECT t.name, COUNT(rt.repo_id) as usage_count
+        FROM topics t
+        JOIN repo_topics rt ON t.id = rt.topic_id
+        GROUP BY t.id, t.name
         ORDER BY usage_count DESC
-        LIMIT 20;
+        LIMIT 40;
     `, []);
-    return rows.map(row => row.topic_name)
+    return rows.map(row => row.name)
 };
 
 const GetProcessedRepositoriesCount = async () => {
@@ -38,4 +45,116 @@ const GetSupportedSources = async () => {
     return rows.map(row => row.name)
 };
 
-export default { GetPopularTags, GetSupportedLanguages, GetProcessedRepositoriesCount, GetSupportedSources};
+
+
+const GetFullRecommendations = async (lang, libs = [], topics = [], sources = [], limit = 10, offset = 0, search = "") => {
+    if (!lang) throw new Error("Language is required");
+    if (!sources || sources.length === 0) return []
+
+    const langLower = lang.toLowerCase();
+    const libsLower = libs.map(l => l.toLowerCase());
+    const topicsLower = topics.map(t => t.toLowerCase());
+    const sourcesLower = sources.map(s => s.toLowerCase());
+
+    let targetReposQuery = knex('repositories as r')
+        .select('r.id')
+        .join('languages as lang', 'r.language_id', 'lang.id')
+        .join('data_sources as ds', 'r.source_id', 'ds.id')
+        .where('lang.name', langLower)
+        .whereIn('ds.name', sourcesLower);
+
+    if (libsLower.length > 0) {
+        targetReposQuery = targetReposQuery.whereIn('r.id', function() {
+            this.select('rl.repo_id')
+                .from('repo_libraries as rl')
+                .join('libraries as l', 'rl.library_id', 'l.id')
+                .whereIn('l.name', libsLower)
+                .groupBy('rl.repo_id')
+                .havingRaw('COUNT(DISTINCT l.id) >= ?', [Math.floor(libsLower.length / 2.0)]);
+        });
+    }
+
+    if (topicsLower.length > 0) {
+        targetReposQuery = targetReposQuery.whereIn('r.id', function() {
+            this.select('rt.repo_id')
+                .from('repo_topics as rt')
+                .join('topics as t', 'rt.topic_id', 't.id')
+                .whereIn('t.name', topicsLower)
+                .groupBy('rt.repo_id')
+                .havingRaw('COUNT(DISTINCT t.id) >= ?', [Math.floor(topicsLower.length / 2.0)])
+        });
+    }
+
+    const query = knex('repo_libraries as rl')
+        .select(
+            'l.name',
+            'ls.final_score as trust_score',
+            knex.raw('COUNT(*) OVER() AS total_count') 
+        )
+        .count('rl.repo_id as co_occurrence_count')
+        .join('libraries as l', 'rl.library_id', 'l.id')
+        .join('target_repos', 'rl.repo_id', 'target_repos.id')
+        .leftJoin('library_scores as ls', 'l.id', 'ls.library_id')
+        .where('l.language_id', function() {
+            this.select('id').from('languages').where('name', langLower).limit(1);
+        })
+        .whereNotIn('l.name', libsLower);
+
+    if (search && search.trim() !== "") {
+        query.where('l.name', 'ILIKE', `%${search.trim().toLowerCase()}%`);
+    }
+
+    query
+        .groupBy('l.id', 'l.name', 'ls.final_score')
+        .orderBy([
+            { column: 'co_occurrence_count', order: 'desc' },
+            { column: 'ls.final_score', order: 'desc' }
+        ])
+        .limit(limit)
+        .offset(offset);
+
+    const finalKnexQuery = knex
+        .with('target_repos', targetReposQuery)
+        .select('*')
+        .from(query);
+
+
+    const { sql, bindings } = finalKnexQuery.toSQL().toNative();
+
+    const result = await executeQuery(sql, bindings);
+
+    return {
+        data: result.map(row => ({
+            name: row.name,
+        })),
+        total: result.length > 0 ? parseInt(result[0].total_count) : 0
+    };
+};
+
+async function GetTagHint(partial, selectedTags = []) {
+    if (!partial) return [];
+    const params = [
+        `%${partial.toLowerCase()}%`, 
+        `${partial.toLowerCase()}%`, 
+        selectedTags, 
+        6
+    ];
+
+    const sql = `
+        SELECT t.name, COUNT(rt.repo_id) as usage_count
+        FROM topics t
+        LEFT JOIN repo_topics rt ON t.id = rt.topic_id
+        WHERE t.name ILIKE $1
+          AND t.name != ALL($3)  -- Исключаем уже выбранные теги
+        GROUP BY t.id, t.name
+        ORDER BY 
+            (t.name ILIKE $2) DESC,
+            usage_count DESC
+        LIMIT $4;
+    `;
+
+    return await executeQuery(sql, params);
+};
+
+export default { GetPopularTags, GetSupportedLanguages, GetProcessedRepositoriesCount, 
+    GetSupportedSources, GetFullRecommendations, GetTagHint };
